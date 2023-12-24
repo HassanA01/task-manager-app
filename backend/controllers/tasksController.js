12238de695ef1f646a87d6fe3c802b162eff85a6
Aneeq <aneeq.hassan@utoronto.ca>
2023-12-24 10:05:34 -0500

    updated error handling and tested all endpoints

--- backend/controllers/tasksController.js
+++ backend/controllers/tasksController.js
@@ -1,6 +1,5 @@
 const { db } = require('../database');
 
-
 // Gets the tasks associated with this project <projectId>
 const getProjectTasks = async (projectId) => {
     try {
@@ -9,40 +8,58 @@ const getProjectTasks = async (projectId) => {
         return result.rows
     } catch (error) {
         console.error(error);
+        throw error;
     }
 };
 
-const addTasks = async (projectId, taskName, taskDesc) => {
+const addTask = async (projectId, taskName, taskDesc) => {
+
     try {
-        await db.query('INSERT INTO tasks (project_id, name, description) VALUES ($1, $2, $3);', [projectId, taskName, taskDesc]);
+
+        if (taskName === "" ) { 
+            throw new Error("name field cannot be empty");
+        }
+
+        await db.query('INSERT INTO tasks (project_id, name, description, status) VALUES ($1, $2, $3, $4);', [projectId, taskName, taskDesc, 'in progress']);
 
         return { message: "Task added successfully"};
     } catch (error) {
         console.error("Error adding task to project:", error.stack );
+        throw error;
     }
 };
 
-const updateTasks = async (projectId, taskId, taskName, taskDesc) => {
+const updateTask = async (projectId, taskId, taskName, taskDesc, taskStatus) => {
 
     try {
-        await db.query('UPDATE tasks SET name = $1, description = $2 WHERE id = $3 AND project_id = $4', [taskName, taskDesc, taskId, projectId]);
+
+        if (taskName === "" ) {
+            throw new Error("name field cannot be empty");
+        }
+
+        await db.query('UPDATE tasks SET name = $1, description = $2, status = $5 WHERE id = $3 AND project_id = $4', [taskName, taskDesc, taskId, projectId, taskStatus]);
 
         return { message : "Task updated successfully"};
     } catch (error) {
+
         console.error("Error updating task", error.stack);
+
+        throw error;
     }
 };
 
-const deleteTask = async (taskId, projectId) => {
+const deleteTask = async (projectId, taskId) => {
+    console.log(projectId, taskId)
     try {
-        await db.query("DELETE FROM tasks WHERE id = $1 and project_id = $2", [taskId, projectId]);
+        await db.query("DELETE FROM tasks WHERE id = $1 AND project_id = $2", [taskId, projectId]);
 
         return {message: "Task deleted successfully"};
     } catch (error) {
         console.error("Error deleting task", error.stack);
+        throw error;
     }
 };
 
 
 
-module.exports = {getProjectTasks, addTasks, updateTasks, deleteTask};
+module.exports = {getProjectTasks, addTask, updateTask, deleteTask};
--- backend/routes/tasks.js
+++ backend/routes/tasks.js
@@ -19,29 +19,31 @@ router.get('/:project_id', async (req, res) => {
 // Route to add a task for a given project
 router.post('/:project_id', async (req, res) => {
     const projectId = req.params.project_id;
-    const {taskName, taskDesc } = req.body;
+    const {taskName, taskDesc} = req.body;
 
     try {
         const result = await tasksController.addTask(projectId, taskName, taskDesc);
-        res.status(200).json(result);
+        res.status(200).json(result);   
     } catch (err) {
         res.status(500).json({message: err.message });
     }
 });
 
-router.patch(':project_id/:task_id', async (req, res) => {
+// Route to update task
+router.patch('/:project_id/:task_id', async (req, res) => {
     const {project_id, task_id} = req.params;
-    const {taskName, taskDesc} = req.body;
+    const {taskName, taskDesc, taskStatus} = req.body;
 
     try {
-        const result = await tasksController.updateTask(project_id, task_id, taskName, taskDesc);
+        const result = await tasksController.updateTask(project_id, task_id, taskName, taskDesc, taskStatus);
         res.status(200).json(result);
-    } catch {
+    } catch (err) {
         res.status(500).json({message: err.message});
     }
 });
 
-router.delete(':project_id/:task_id', async (req, res) => {
+// Delete a task
+router.delete('/:project_id/:task_id', async (req, res) => {
 
     const {project_id, task_id} = req.params;
 
