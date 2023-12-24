c087691b465030432fb3c4cf2cd9e2dae40778b2
Harshil-Patel28 <76663814+Harshil-Patel28@users.noreply.github.com>
2023-12-23 23:16:17 -0500

    Added an update controller and route for projects.

--- backend/database.js
+++ backend/database.js
@@ -1,14 +1,19 @@
 const pg = require('pg');
 
-console.log(typeof process.env.DB_PASSWORD)
 const db = new pg.Client({
     user: process.env.DB_USER || 'postgres',
     host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'your_default_db_name', 
+    database: process.env.DB_NAME || 'postgres', 
     port: process.env.DB_PORT || 5432, 
-    password: process.env.DB_PASSWORD
+    password: process.env.DB_PASSWORD || 'default_password' // Use environment variable or default password
 });
 
-db.connect();
+db.connect()
+    .then(() => {
+        console.log('Connected to the database'); // Log when the database connection is established
+    })
+    .catch(err => {
+        console.error('Error connecting to the database:', err);
+    });
 
 module.exports = { db };
